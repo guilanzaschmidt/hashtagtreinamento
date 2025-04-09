5807cf4af4f003f94141780e863faae4f24282a4
Guilherme Schmidt <glschmidt@baldan.com.br>
2025-04-09 17:08:44 -0300

    Aula 1

--- Codigo.py
+++ Codigo.py
@@ -1,6 +1,13 @@
 import pyautogui
+import time
 
+pyautogui.PAUSE = 0.5
 
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+time.sleep(3)
